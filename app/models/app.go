2b4857c40df0930d2e716c50d2f7becda8ac7b14
François Chastanet <fchastanet@gmail.com>
2025-04-15 19:07:59 +0200

    feat: Refactor help model integration and create a dedicated help component

--- app/models/app.go
+++ app/models/app.go
@@ -3,17 +3,13 @@ package models
 import (
 	"strings"
 
-	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/fchastanet/shell-command-bookmarker/internal/components/help"
 	"github.com/fchastanet/shell-command-bookmarker/internal/components/tabs"
 	"github.com/fchastanet/shell-command-bookmarker/internal/framework/focus"
 )
-
-type settings struct {
-	keys keyMap
-}
 
 type styles struct {
 	docStyle       lipgloss.Style
@@ -22,13 +18,13 @@ type styles struct {
 }
 
 type AppModel struct {
-	width         int
-	height        int
-	help          *help.Model
+	width  int
+	height int
+	styles *styles
+	// sub components
+	appHelpModel  *help.Model
 	TabsComponent *tabs.Tabs
 	FocusManager  *focus.Manager
-	settings      *settings
-	styles        *styles
 }
 
 func NewAppModel(
@@ -56,67 +52,37 @@ func NewAppModel(
 		&styles.highlightColor,
 		&styles.windowStyle,
 	)
+	appHelpModel := help.NewAppHelpModel(
+		focusManager,
+		&styles.docStyle,
+	)
 
-	helpModel := help.New()
 	m := AppModel{
 		width:         0,
 		height:        0,
-		help:          &helpModel,
+		appHelpModel:  &appHelpModel,
 		TabsComponent: tabsModel,
 		FocusManager:  focusManager,
-		settings: &settings{
-			keys: defaultKeyMap(),
-		},
-		styles: styles,
+		styles:        styles,
 	}
+
+	shortHelp := func() []key.Binding {
+		return m.appHelpModel.GetKeyBindings()
+	}
+
+	fullHelp := func() [][]key.Binding {
+		helpKeyMap := m.appHelpModel.GetKeyBindings()
+		return [][]key.Binding{
+			helpKeyMap,
+			m.FocusManager.GetKeyBindings(),
+			m.TabsComponent.GetKeyBindings(),
+		}
+	}
+
+	m.appHelpModel.SetShortHelpHandler(shortHelp)
+	m.appHelpModel.SetFullHelpHandler(fullHelp)
+
 	return m
-}
-
-// keyMap defines a set of keybindings. To work for help it must satisfy
-// key.Map. It could also very easily be a map[string]key.Binding.
-type keyMap struct {
-	Up   key.Binding
-	Down key.Binding
-	Help key.Binding
-	Quit key.Binding
-}
-
-// ShortHelp returns keybindings to be shown in the mini help view. It's part
-// of the key.Map interface.
-func (m AppModel) ShortHelp() []key.Binding {
-	return []key.Binding{m.settings.keys.Help, m.settings.keys.Quit}
-}
-
-// FullHelp returns keybindings for the expanded help view. It's part of the
-// key.Map interface.
-func (m AppModel) FullHelp() [][]key.Binding {
-	return [][]key.Binding{
-		{m.settings.keys.Up, m.settings.keys.Down}, // first column
-		m.FocusManager.GetKeyBindings(),
-		m.TabsComponent.GetKeyBindings(),
-		{m.settings.keys.Help, m.settings.keys.Quit}, // last column
-	}
-}
-
-func defaultKeyMap() keyMap {
-	return keyMap{
-		Up: key.NewBinding(
-			key.WithKeys("up", "k"),
-			key.WithHelp("↑/k", "move up"),
-		),
-		Down: key.NewBinding(
-			key.WithKeys("down", "j"),
-			key.WithHelp("↓/j", "move down"),
-		),
-		Help: key.NewBinding(
-			key.WithKeys("?", "h"),
-			key.WithHelp("?/h", "toggle help"),
-		),
-		Quit: key.NewBinding(
-			key.WithKeys("q", "esc", "ctrl+c"),
-			key.WithHelp("q/Escape", "quit"),
-		),
-	}
 }
 
 func (m AppModel) IsFocusable() bool {
@@ -136,6 +102,7 @@ func (m AppModel) GetFocusableUniqueID() string {
 func (m AppModel) Init() tea.Cmd {
 	// Initialize sub-models
 	return tea.Batch(
+		m.appHelpModel.Init(),
 		m.FocusManager.Init(),
 		m.TabsComponent.Init(),
 	)
@@ -155,22 +122,16 @@ func (m AppModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	m.TabsComponent = &tabsModel
 	cmds = append(cmds, cmd)
 
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		// If we set a width on the help menu it can gracefully truncate
-		// its view as needed.
-		m.help.Width = msg.Width
+	// Update help model
+	helpModel, cmd := m.appHelpModel.Update(msg)
+	helpModelInstance := helpModel.(help.Model)
+	m.appHelpModel = &helpModelInstance
+	cmds = append(cmds, cmd)
+
+	if msg, ok := msg.(tea.WindowSizeMsg); ok {
 		m.width = msg.Width
 		m.height = msg.Height
-	case tea.KeyMsg:
-		switch {
-		case key.Matches(msg, m.settings.keys.Quit):
-			cmds = append(cmds, tea.Quit)
-		case key.Matches(msg, m.settings.keys.Help):
-			m.help.ShowAll = !m.help.ShowAll
-		}
 	}
-
 	return m, tea.Batch(cmds...)
 }
 
@@ -196,17 +157,9 @@ func getDefaultStyles(
 func (m AppModel) View() string {
 	doc := strings.Builder{}
 
-	helpView := m.help.View(m)
-	height := 0
-	if m.help.ShowAll {
-		height = strings.Count(helpView, "\n")
-	} else {
-		height = 2
-	}
-
 	renderedTabs := m.TabsComponent.View()
 	tabsStr := lipgloss.JoinHorizontal(lipgloss.Top, renderedTabs)
 	doc.WriteString(tabsStr)
-	doc.WriteString(strings.Repeat("\n", height) + helpView)
+	doc.WriteString(m.appHelpModel.View())
 	return m.styles.docStyle.Render(doc.String())
 }
---
+++ internal/components/help/help.go
@@ -0,0 +1,150 @@
+package help
+
+import (
+	"strings"
+
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/fchastanet/shell-command-bookmarker/internal/framework/focus"
+)
+
+type Model struct {
+	width            int
+	height           int
+	help             *help.Model
+	shortHelpHandler func() []key.Binding
+	fullHelpHandler  func() [][]key.Binding
+	focusManager     *focus.Manager
+	settings         *settings
+	styles           *styles
+}
+
+type styles struct {
+	docStyle *lipgloss.Style
+}
+
+// keyMap defines a set of keybindings. To work for help it must satisfy
+// key.Map. It could also very easily be a map[string]key.Binding.
+type keyMap struct {
+	Help key.Binding
+	Quit key.Binding
+}
+
+type settings struct {
+	keys keyMap
+}
+
+type KeyBindingsHelpInterface interface {
+	ShortHelp() []key.Binding
+	FullHelp() [][]key.Binding
+}
+
+func defaultKeyMap() keyMap {
+	return keyMap{
+		Help: key.NewBinding(
+			key.WithKeys("?", "h"),
+			key.WithHelp("?/h", "toggle help"),
+		),
+		Quit: key.NewBinding(
+			key.WithKeys("q", "esc", "ctrl+c"),
+			key.WithHelp("q/Escape", "quit"),
+		),
+	}
+}
+
+func (m Model) GetKeyBindings() []key.Binding {
+	return []key.Binding{
+		m.settings.keys.Help,
+		m.settings.keys.Quit,
+	}
+}
+
+func NewAppHelpModel(
+	focusManager *focus.Manager,
+	docStyle *lipgloss.Style,
+) Model {
+	helpModel := help.New()
+	m := Model{
+		width:        0,
+		height:       0,
+		help:         &helpModel,
+		focusManager: focusManager,
+		shortHelpHandler: func() []key.Binding {
+			return []key.Binding{}
+		},
+		fullHelpHandler: func() [][]key.Binding {
+			return [][]key.Binding{}
+		},
+		settings: &settings{
+			keys: defaultKeyMap(),
+		},
+		styles: &styles{
+			docStyle: docStyle,
+		},
+	}
+	return m
+}
+
+func (m *Model) SetShortHelpHandler(
+	shortHelpHandler func() []key.Binding,
+) {
+	m.shortHelpHandler = shortHelpHandler
+}
+
+func (m *Model) SetFullHelpHandler(
+	fullHelpHandler func() [][]key.Binding,
+) {
+	m.fullHelpHandler = fullHelpHandler
+}
+
+func (m Model) Init() tea.Cmd {
+	// Initialize sub-models
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		// If we set a width on the help menu it can gracefully truncate
+		// its view as needed.
+		m.help.Width = msg.Width
+		m.width = msg.Width
+		m.height = msg.Height
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.settings.keys.Help):
+			m.help.ShowAll = !m.help.ShowAll
+		case key.Matches(msg, m.settings.keys.Quit):
+			cmds = append(cmds, tea.Quit)
+		}
+	}
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m *Model) ShortHelp() []key.Binding {
+	return m.shortHelpHandler()
+}
+
+func (m *Model) FullHelp() [][]key.Binding {
+	return m.fullHelpHandler()
+}
+
+func (m Model) View() string {
+	doc := strings.Builder{}
+
+	helpView := m.help.View(&m)
+	height := 0
+	if m.help.ShowAll {
+		height = strings.Count(helpView, "\n")
+	} else {
+		height = 2
+	}
+
+	doc.WriteString(strings.Repeat("\n", height) + helpView)
+	return m.styles.docStyle.Render(doc.String())
+}
