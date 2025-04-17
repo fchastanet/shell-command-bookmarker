4af5ca4be14eed7d4beafe4ec0a71b0c46748632
François Chastanet <fchastanet@gmail.com>
2025-04-17 17:13:28 +0200

    get default history file if env HISTFILE not set

--- app/services/HistoryService.go
+++ app/services/HistoryService.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"log/slog"
 	"os"
+	"path/filepath"
 
 	"github.com/fchastanet/shell-command-bookmarker/app/processors"
 )
@@ -28,10 +29,23 @@ func NewHistoryService(
 	}
 }
 
-func getHistoryFilePath() (string, error) {
-	historyFile := os.Getenv("HISTFILE")
-	if historyFile != "" {
-		_, err := os.Stat(historyFile)
+func getHomeDir() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		slog.Error("Error getting home directory", "error", err)
+		return "", err
+	}
+	return homeDir, nil
+}
+
+func getDefaultHistoryFilePath() (string, error) {
+	homeDir, err := getHomeDir()
+	if err != nil {
+		slog.Error("Error getting home directory", "error", err)
+		return "", err
+	}
+	historyFile := filepath.Join(homeDir, ".bash_history")
+	if _, err := os.Stat(historyFile); err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			slog.Error("History file does not exist", "file", historyFile, "error", err)
 			return "", err
@@ -40,9 +54,31 @@ func getHistoryFilePath() (string, error) {
 			slog.Error("Permission denied to access history file", "file", historyFile, "error", err)
 			return "", err
 		}
+	}
+	return historyFile, nil
+}
+
+func getHistoryFilePath() (string, error) {
+	historyFile := os.Getenv("HISTFILE")
+	if historyFile == "" {
+		historyFile, err := getDefaultHistoryFilePath()
+		if err != nil {
+			slog.Error("Error getting default history file path", "error", err)
+			return "", err
+		}
+		slog.Info("Using default history file path", "file", historyFile)
 		return historyFile, nil
 	}
-	return "", nil
+	_, err := os.Stat(historyFile)
+	if errors.Is(err, os.ErrNotExist) {
+		slog.Error("History file does not exist", "file", historyFile, "error", err)
+		return "", err
+	}
+	if errors.Is(err, os.ErrPermission) {
+		slog.Error("Permission denied to access history file", "file", historyFile, "error", err)
+		return "", err
+	}
+	return historyFile, nil
 }
 
 func (s *HistoryService) IngestHistory() error {
